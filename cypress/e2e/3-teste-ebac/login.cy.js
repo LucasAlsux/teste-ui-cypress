3af3859ab1ded7e5eb6862e7b2fc4b42c9df5c1a
Lucas Alsux <lucas.alsux@gmail.com>
2025-03-24 10:07:56 -0300

    adicionado novo teste de login, massa de dados e adição no .gitignore

--- cypress/e2e/3-teste-ebac/login.cy.js
+++ cypress/e2e/3-teste-ebac/login.cy.js
@@ -1,7 +1,9 @@
 /// <reference types="cypress" />
+    
+const perfil = require('../../fixtures/dados.json')
 
     describe('Teste do sistema de login', () => {
-        
+      
         beforeEach(() => {
             cy.visit('minha-conta')           
         });
@@ -33,4 +35,12 @@
             
         });
 
-    })
+        it.skip('Deve fazer login com sucesso - usando massa de dados', () => {
+        
+            cy.get('#username').type(perfil.usuario)
+            cy.get('#password').type(perfil.senha)
+            cy.get('.woocommerce-form > .button').click()
+            cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, lolo (não é lolo? Sair)')
+            })
+        
+        })
